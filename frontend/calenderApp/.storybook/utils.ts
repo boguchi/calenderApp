7db634dd7764756c5d134bbedabe2bcb186d26f7
boguchi <teradachiyuu@teradachiyuunoMacBook-Pro.local>
2024-09-17 21:04:30 +0900

    docs追加

--- frontend/calenderApp/.storybook/utils.ts
+++ frontend/calenderApp/.storybook/utils.ts
@@ -1,5 +1,17 @@
 import { http, HttpResponse } from 'msw'
 
+/**
+ * mswのhandlersを作成
+ * @param url - リクエスト先のURL
+ * @param data - mock用のデータ
+ *
+ * @see 使用例\
+ * parameters: {\
+ * msw: {\
+ * handlers: getHttpResponse('url', data)\
+ * }\
+ * }
+ */
 export const getHttpResponse = (url: string, data: any) => {
   return [
     http.get(url, () => {
