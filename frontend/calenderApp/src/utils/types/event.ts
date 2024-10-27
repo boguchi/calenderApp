190f1d593893b11ec17d939465e8c05a15e21f02
boguchi <teradachiyuu@teradachiyuunoMacBook-Pro.local>
2024-10-27 22:16:41 +0900

    eventの型ファイル作成

---
+++ frontend/calenderApp/src/utils/types/event.ts
@@ -0,0 +1,9 @@
+export type EventItemType = {
+  title: string
+  startDate: Date
+  endDate: Date
+  startTime?: string
+  endTime?: string
+  labelColor: 'red' | 'blue' | 'green'
+  memo?: string
+}
