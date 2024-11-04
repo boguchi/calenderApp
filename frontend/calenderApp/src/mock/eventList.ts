faaede8b49bab326c0c9ac563008331f51173a63
boguchi <teradachiyuu@teradachiyuunoMacBook-Pro.local>
2024-11-04 23:10:02 +0900

    mockデータ修正

--- frontend/calenderApp/src/mock/eventList.ts
+++ frontend/calenderApp/src/mock/eventList.ts
@@ -9,42 +9,42 @@ export const mockEventList: EventItemType[] = [
     id: '1',
     title: 'hoge',
     startDate: new Date(thisYear, thisMonthIndex, 1),
-    endDate: new Date(),
+    endDate: new Date(thisYear, thisMonthIndex, 1),
     labelColor: 'red'
   },
   {
     id: '2',
     title: 'hoge',
     startDate: new Date(thisYear, thisMonthIndex, 8),
-    endDate: new Date(),
+    endDate: new Date(thisYear, thisMonthIndex, 8),
     labelColor: 'green'
   },
   {
     id: '3',
     title: 'hoge',
     startDate: new Date(thisYear, thisMonthIndex, 13),
-    endDate: new Date(),
+    endDate: new Date(thisYear, thisMonthIndex, 13),
     labelColor: 'blue'
   },
   {
     id: '4',
     title: 'hoge',
     startDate: new Date(thisYear, thisMonthIndex, 19),
-    endDate: new Date(),
+    endDate: new Date(thisYear, thisMonthIndex, 19),
     labelColor: 'blue'
   },
   {
     id: '5',
     title: 'hoge',
     startDate: new Date(thisYear, thisMonthIndex, 19),
-    endDate: new Date(),
+    endDate: new Date(thisYear, thisMonthIndex, 19),
     labelColor: 'green'
   },
   {
     id: '6',
     title: 'hoge',
     startDate: new Date(thisYear, thisMonthIndex, 25),
-    endDate: new Date(),
+    endDate: new Date(thisYear, thisMonthIndex, 25),
     labelColor: 'red'
   }
 ]
